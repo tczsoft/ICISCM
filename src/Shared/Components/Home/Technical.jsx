5bf64f22f6eb5b175612dcedb9401a52f63b912e
vetri <vetrivel.tcz@gmail.com>
2025-06-24 14:52:30 +0530

    updtes

--- src/Shared/Components/Home/Technical.jsx
+++ src/Shared/Components/Home/Technical.jsx
@@ -5,7 +5,7 @@ function Technical({ Name, Position, location }) {
             {/* Corner Cut Shape */}
             <div className='grid grid-cols-2 items-center bg-[#269C53]  '>
                 <div className='p-5'>
-                    <img src="/assets/Images/home6.jpg" className='w-full h-full col-span-1 rounded-xl ' alt="" />
+                    <img rel="preload" src="/assets/Images/home6.jpg" className='w-full h-full col-span-1 rounded-xl ' alt="" />
                 </div>
                 <div className="flex flex-col items-center gap-3 z-10 mx-auto bg-gradient-to-r from-[#269C53] to-[#306042] justify-center col-span-1 w-full h-full">
                     <div className=" text-white font-semibold  text-sm md:text-base ">
